a20074d0841e84ac0470b494a112655184249592
sameerdr <marathiinsta37@gmail.com>
2025-12-07 12:16:07 +0530

    Add Constants class

---
+++ app/src/main/java/com/railway/cablejoint/Constants.java
@@ -0,0 +1,23 @@
+package com.railway.cablejoint;
+
+public class Constants {
+    public static final String GOOGLE_SHEET_ID = "1hjViC-yDssz7KPelBYr-2AEDQXm7BU08VG1GyFkB--w";
+    public static final String SHEET_NAME = "Joint Data";
+    public static final String RANGE = "A:N";
+    
+    // Column indices
+    public static final int COL_DATE = 0;
+    public static final int COL_SECTION = 1;
+    public static final int COL_SUB_SECTION = 2;
+    public static final int COL_KM = 3;
+    public static final int COL_OHE_MAST = 4;
+    public static final int COL_OFFSET = 5;
+    public static final int COL_CABLE_TYPE = 6;
+    public static final int COL_JOINT_TYPE = 7;
+    public static final int COL_PHOTO = 8;
+    public static final int COL_LATITUDE = 9;
+    public static final int COL_LONGITUDE = 10;
+    public static final int COL_REASON = 11;
+    public static final int COL_STAFF = 12;
+    public static final int COL_REMARK = 13;
+}
